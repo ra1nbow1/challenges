9a7b7351c22bf40ce5a172e10e6bc973c6e695c1
Matvey Romanov <youtuberm@yandex.ru>
2024-07-09 13:44:43 +0300

    Аналитика

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import ProblemsList from './Pages/ProblemsList'
 import Problem from './Pages/Problem'
 import './Styles/App.scss'
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
 	return (
+		<>
+		<Analytics />
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<ProblemsList />} />
 				<Route path="/problem/:problem_id" element={<Problem />} />
 			</Routes>
 		</BrowserRouter>
+		</>
 	)
 }
 
